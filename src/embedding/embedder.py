b247ecd8e3ddd93b9d94e744cb71aae59ae75c42
Hieu <hieuasroma@gmail.com>
2025-11-01 20:18:50 +0700

    "LOL"

--- src/embedding/embedder.py
+++ src/embedding/embedder.py
@@ -1,8 +1,6 @@
+# embedder.py
 from FlagEmbedding import BGEM3FlagModel
 import torch
-import torch.nn.functional as F
-from torch import Tensor
-from transformers import AutoTokenizer, AutoModel
 import numpy as np
 
 class AverageEmbedder:
@@ -18,13 +16,17 @@ class AverageEmbedder:
         # ===== Load model 1 lần =====
         self.bge_m3_model = BGEM3FlagModel('BAAI/bge-m3', use_fp16=True, device=self.device.type)
 
-    @staticmethod
-    def average_pool(last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
-        last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
-        return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+    def embed(self, documents: str | list[str]) -> np.ndarray:
+        if isinstance(documents, str):
+            documents = [documents]
+            single = True
+        else:
+            single = False
 
-    def embed(self, document: str) -> np.ndarray:
-        # ===== BGE-M3 =====
-        bge_m3_embedding_vector = self.bge_m3_model.encode(document, batch_size=12, max_length=8192)['dense_vecs']
-        bge_m3_embedding_vector = np.array(bge_m3_embedding_vector, dtype=np.float32).squeeze()
-        return bge_m3_embedding_vector.astype(np.float32)
+        embedding_vectors = self.bge_m3_model.encode(documents, batch_size=12, max_length=8192)['dense_vecs']
+        embedding_vectors = np.array(embedding_vectors, dtype=np.float32)
+
+        if single:
+            return embedding_vectors[0]
+        else:
+            return embedding_vectors
